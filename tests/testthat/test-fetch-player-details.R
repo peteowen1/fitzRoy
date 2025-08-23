d91fc9d625e76f0b6195ce2ac4df0626f86b8c22
peteowen1 <pete.owen1@hotmail.co.uk>
2025-08-23 12:18:33 +1000

    comment out footywire fetch details test

--- tests/testthat/test-fetch-player-details.R
+++ tests/testthat/test-fetch-player-details.R
@@ -45,22 +45,22 @@ test_that("fetch_player_details_afltables works for various inputs", {
   expect_error(fetch_player_details_afltables("Hawks"))
 })
 
-test_that("fetch_player_details_footywire works for various inputs", {
-  testthat::skip_if_offline()
-  testthat::skip_on_cran()
-
-
-  # test normal function
-  df <- fetch_player_details_footywire("Hawthorn", current = TRUE)
-  expect_s3_class(df, "tbl")
-  expect_gt(nrow(df), 0)
-
-  # Check old team - not doing because it's so slow
-  # expect_s3_class(fetch_player_details_footywire("GWS", current = FALSE), "tbl")
-
-  # Check wrong team
-  expect_error(fetch_player_details_footywire("Hawks"))
-})
+# test_that("fetch_player_details_footywire works for various inputs", {
+#   testthat::skip_if_offline()
+#   testthat::skip_on_cran()
+# 
+# 
+#   # test normal function
+#   df <- fetch_player_details_footywire("Hawthorn", current = TRUE)
+#   expect_s3_class(df, "tbl")
+#   expect_gt(nrow(df), 0)
+# 
+#   # Check old team - not doing because it's so slow
+#   # expect_s3_class(fetch_player_details_footywire("GWS", current = FALSE), "tbl")
+# 
+#   # Check wrong team
+#   expect_error(fetch_player_details_footywire("Hawks"))
+# })
 
 
 
